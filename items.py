99499ca35ebf917567bc82a24531d53a85e3b964
HimangsuBorah <himangsuborah255@gmail.com>
2024-06-26 09:16:48 +0530

    first commit

---
+++ items.py
@@ -0,0 +1,23 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class BookscrapperItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    pass
+
+class BookItem(scrapy.item):
+    url=scrapy.Field()
+    title=scrapy.Field()
+    upc=scrapy.Field()
+    product_type=scrapy.Field()
+    price_excl_tax=scrapy.Field()
+    price_incl_tax=scrapy.Field()
+    tax=scrapy.Field()
+    availability
+
